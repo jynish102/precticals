80f41626bb233cd1041cf2ed1886093e3575ffe1
jynish102 <jaykal67kal@gmail.com>
2025-12-06 23:19:05 +0530

    add all

--- sidebar.js
+++ sidebar.js
@@ -1,7 +1,7 @@
 const body = document.querySelector("body");
 const sidebar = document.querySelector(".sidebar");
 const toggle = document.querySelector(".toggle");
-const modeSwitch = document.querySelector(".toggle-switch");
+
 const modeText = document.querySelector(".mode-text");
 
 toggle.addEventListener("click", () => {
